be48be8f81bd615c065abc2bd4c50963f663725c
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-23 11:36:06 -0600

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    total = 0
+    # what punctuation we're checking for
+    special_characters = ['.', '!', '?']
+    # iterate over every character in the string
+    self.chars.each.with_index do |char, index|
+      # check if the character is a punctuation, and if the _next_ character is _not_ a punctuation
+      total += 1 if special_characters.include?(char) && !special_characters.include?(self[index + 1])
+    end
+    total
   end
 end
